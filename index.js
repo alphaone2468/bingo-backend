fd245aa11db21f9f58120a7b6195e104590806fd
Ahmed <129504534+alphaone2468@users.noreply.github.com>
2024-10-25 16:24:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server=http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"https://bingo-game101.netlify.app/",
+        origin:"https://bingo-game101.netlify.app",
     }
 
 });
